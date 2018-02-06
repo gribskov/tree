16447d17474cdd482e0a3a10bf80f6506295b01c
Michael Gribskov <mgribsko@gmail.com>
2018-02-05 20:00:47 -0500

    tree.py - basic tree structure

--- tree.py
+++ tree.py
@@ -3,14 +3,34 @@ Tree
 
 General tree structure with multiple branches
 ================================================================================================="""
+
+
 class Tree:
 
-    def __init__(self):
-        """
+    def __init__(self, id=''):
+        """-----------------------------------------------------------------------------------------
+        Tree constructor
+        :param id: string, the id of the new node
+        -----------------------------------------------------------------------------------------"""
+        self.id = id
+        self.children = []
 
+    def childAdd(self, subtree):
+        """-----------------------------------------------------------------------------------------
+        Add a child tree to the children list
+        :param subtree: a Tree object
         """
-        pass
+        self.children.append(subtree)
+        return None
 
 
 if __name__ == '__main__':
+    root = Tree('root')
+    a = Tree('a')
+    root.childAdd(a)
+    b = Tree('b')
+    root.childAdd(b)
+
+    print(root)
+
     exit(0)
