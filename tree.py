c9e05418714750083858abd197532e18f0e1c2f7
gribskov <mgribskov@gmail.com>
2018-02-07 12:07:19 -0500

    added newickLoad() method to build tree from Newick string.

--- tree.py
+++ tree.py
@@ -117,6 +117,66 @@ class Tree:
 
         return newick
 
+    def newickLoad(self, newick):
+        """-----------------------------------------------------------------------------------------
+        Builds a tree frm a newick string.  Here are some examples from Joe Felsenstein's website
+        http://evolution.genetics.washington.edu/phylip/newicktree.html. These are the test trees used below
+
+        unrooted tree (three central branches)
+        ((raccoon:19.2,bear:6.8):0.9,((sea_lion:13.0, seal:12.0):7.5,((monkey:100.9,cat:47.1):20.6, weasel:18.9):2.09):3.9,dog:25.5);
+
+        rooted tree
+        (Bovine:0.7,(Gibbon:0.4,(Orang:0.3,(Gorilla:0.2,(Chimp:0.2, Human:0.1):0.1):0.1):0.2):0.5,Mouse:1.2):0.1;
+
+        another unrooted tree
+        (Bovine:0.69,(Hylobates:0.36,(Pongo:0.34,(G._Gorilla:0.17, (P._paniscus:0.19,H._sapiens:0.12):0.08):0.06):0.15):0.55, Rodent:1.21);
+
+        USAGE
+           $root.newickLoad( newick_string );
+
+        :param newick: a newick formatted string defining a tree
+        :return: True, if successful
+        -----------------------------------------------------------------------------------------"""
+        newick = newick.strip()     # removes white space
+        newick = newick.rstrip(';') # removes semicolon if present
+        stack = [self]
+        node = self
+        word = ''
+        if newick[0] == '(':
+            # newick = newick.replace('(', '', 1)
+            pass
+        else:
+            print('Tree.newickLoad - Newick string must begin with (')
+            return False
+
+        for letter in newick:
+            if letter == '(':
+                newnode = Tree()
+                node.childAdd(newnode)
+                stack.append(node)
+                node = newnode
+                word = ''
+
+            elif letter == ',':
+                node.name = word
+                node = stack[-1]
+                newnode = Tree()
+                node.childAdd(newnode)
+                node = newnode
+                word = ''
+
+            elif letter == ')':
+                node.name = word
+                node = stack.pop()
+                word = ''
+
+            else:
+                word += letter
+        # when you finish, if there is anything in word it belongs to the root
+        self.name = word
+
+        return True
+
 
 # --------------------------------------------------------------------------------------------------
 # Testing
@@ -175,4 +235,20 @@ if __name__ == '__main__':
     b.childAdd(f)
 
     print(root.newick())
+
+    print('\nNewick format:')
+    trees = [
+        '((a,b),(c,d,e),f)',
+        '((raccoon:19.2, bear:6.8):0.9, ((sea_lion:13.0, seal:12.0):7.5, ((monkey:100.9, cat:47.1):20.6, weasel:18.9): 2.09):3.9, dog: 25.5);',
+        '(Bovine: 0.7, (Gibbon:0.4, (Orang:0.3, (Gorilla:0.2, (Chimp:0.2, Human:0.1): 0.1):0.1):0.2):0.5, Mouse: 1.2):0.1\n',
+        '(Bovine: 0.69, (Hylobates:0.36, (Pongo:0.34, (G._Gorilla:0.17, (P._paniscus:0.19, H._sapiens:0.12): 0.08):0.06):0.15):0.55, Rodent: 1.21)'
+    ]
+    for tree_string in trees:
+
+        print('tree in:', tree_string)
+        root = Tree()
+        root.newickLoad(tree_string)
+        print('tree out:', root.newick())
+
+
     exit(0)
