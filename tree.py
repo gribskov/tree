aa56ecc80105711c4e80a2f51912b1fcdbbafa8d
gribskov <mgribskov@gmail.com>
2018-02-07 12:32:11 -0500

    Updated all documentation.  Added test for spaces in newickLoad()

--- tree.py
+++ tree.py
@@ -1,7 +1,15 @@
 """=================================================================================================
 Tree
+General tree structure with multiple branches.
 
-General tree structure with multiple branches
+Synopsis:
+    root = Tree()           # create an empty tree node
+    child = Tree('a')       # create a node named 'a'
+    root.chilAdd(child)    # add child node to tree
+
+    root = Tree()
+    root.newickLoad('((a,b),c)'     # create tree from Newick string
+    print(root.newick+';')          # print out newick formatted tree (note the semicolon)
 ================================================================================================="""
 
 
@@ -11,6 +19,7 @@ class Tree:
 
         """-----------------------------------------------------------------------------------------
         Tree constructor
+        TODO: add branch length and bootstrap
         :param id: string, the id of the new node
         -----------------------------------------------------------------------------------------"""
         self.name = name
@@ -19,14 +28,14 @@ class Tree:
 
     def __iter__(self):
         """-----------------------------------------------------------------------------------------
-        The iterator is the depth firts search generator function.
+        The iterator is the depth first search generator function.
         __next__ is not needed - it is supplied by the generator
         -----------------------------------------------------------------------------------------"""
         return self.iterator
 
     def childAdd(self, subtree):
         """-----------------------------------------------------------------------------------------
-        Add a child tree to the children list
+        Add a child tree to the children list of this node.
         :param subtree: a Tree object
         """
         self.children.append(subtree)
@@ -34,8 +43,7 @@ class Tree:
 
     def order(self):
         """-----------------------------------------------------------------------------------------
-        traverse the tree in the current mode order, dfs or bfs.
-        kind of a dummy function since it does no more thant print
+        return a list of nodes that traverse the tree in the current mode order, dfs or bfs.
         :return: list of nodes
         -----------------------------------------------------------------------------------------"""
         nodelist = [self]
@@ -103,6 +111,7 @@ class Tree:
                |--
         c -----|
         ((a,b),c)
+        TODO: need to add branch length and bootstrap
         :return: newick formatted tree
         -----------------------------------------------------------------------------------------"""
         newick = ''
@@ -119,7 +128,7 @@ class Tree:
 
     def newickLoad(self, newick):
         """-----------------------------------------------------------------------------------------
-        Builds a tree frm a newick string.  Here are some examples from Joe Felsenstein's website
+        Builds a tree from a newick string.  Here are some examples from Joe Felsenstein's website
         http://evolution.genetics.washington.edu/phylip/newicktree.html. These are the test trees used below
 
         unrooted tree (three central branches)
@@ -132,13 +141,13 @@ class Tree:
         (Bovine:0.69,(Hylobates:0.36,(Pongo:0.34,(G._Gorilla:0.17, (P._paniscus:0.19,H._sapiens:0.12):0.08):0.06):0.15):0.55, Rodent:1.21);
 
         USAGE
-           $root.newickLoad( newick_string );
+           root.newickLoad( newick_string );
 
         :param newick: a newick formatted string defining a tree
         :return: True, if successful
         -----------------------------------------------------------------------------------------"""
-        newick = newick.strip()     # removes white space
-        newick = newick.rstrip(';') # removes semicolon if present
+        newick = newick.strip()  # removes white space
+        newick = newick.rstrip(';')  # removes semicolon if present
         stack = [self]
         node = self
         word = ''
@@ -150,7 +159,9 @@ class Tree:
             return False
 
         for letter in newick:
-            if letter == '(':
+            if letter.isspace():
+                continue
+            elif letter == '(':
                 newnode = Tree()
                 node.childAdd(newnode)
                 stack.append(node)
@@ -172,6 +183,7 @@ class Tree:
 
             else:
                 word += letter
+
         # when you finish, if there is anything in word it belongs to the root
         self.name = word
 
@@ -183,6 +195,7 @@ class Tree:
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
+    print('Manual tree building')
     root = Tree('root')
     a = Tree('a')
     b = Tree('b')
@@ -202,20 +215,21 @@ if __name__ == '__main__':
     print('root:', root)
 
     print('\nbreadth first search (using mode)')
+    print('    set mode to bfs')
     root.mode('bfs')
     for node in root:
-        print('bfs name:', node.name)
+        print('    bfs name:', node.name)
 
     print('\ndepth first search (using mode)')
+    print('    set mode to dfs')
     root.mode('dfs')
     for node in root:
-        print('name:', node.name)
+        print('    name:', node.name)
 
-    print('\nNewick')
-
+    print('\nNewick: write simple tree from previous tests')
     print(root.newick())
 
-    print('\nNewick 2 - unnamed internal nodes with trifucation')
+    print('\nNewick - tree with unnamed internal nodes with trifucation')
     root = Tree('')
     a = Tree('')
     b = Tree('')
@@ -236,19 +250,17 @@ if __name__ == '__main__':
 
     print(root.newick())
 
-    print('\nNewick format:')
+    print('\nNewick format - read and write test trees from Felsenstein:')
     trees = [
-        '((a,b),(c,d,e),f)',
+        '((a,b),(c,d,e),f);',
         '((raccoon:19.2, bear:6.8):0.9, ((sea_lion:13.0, seal:12.0):7.5, ((monkey:100.9, cat:47.1):20.6, weasel:18.9): 2.09):3.9, dog: 25.5);',
         '(Bovine: 0.7, (Gibbon:0.4, (Orang:0.3, (Gorilla:0.2, (Chimp:0.2, Human:0.1): 0.1):0.1):0.2):0.5, Mouse: 1.2):0.1\n',
         '(Bovine: 0.69, (Hylobates:0.36, (Pongo:0.34, (G._Gorilla:0.17, (P._paniscus:0.19, H._sapiens:0.12): 0.08):0.06):0.15):0.55, Rodent: 1.21)'
     ]
     for tree_string in trees:
-
         print('tree in:', tree_string)
         root = Tree()
         root.newickLoad(tree_string)
         print('tree out:', root.newick())
-
 
     exit(0)
